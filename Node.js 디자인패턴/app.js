80f11494967cba9cec7e010e2320039abb2da895
LKH <gorloom6425@naver.com>
2021-02-01 13:11:08 +0900

    Node.js 디자인패턴 ch 10 (3)

--- Node.js 디자인패턴/app.js
+++ Node.js 디자인패턴/app.js
@@ -5,6 +5,6 @@ http.createServer((req, res) => {
     for (let i = 1e7; i > 0; i--) {}
     console.log(`Handling request from ${pid}`);
     res.end(`Hello from ${pid}\n`);
-}).listen(8080, () => {
+}).listen(process.env.PORT || process.argv[2] || 8080, () => {
     console.log(`Started ${pid}`);
 });
