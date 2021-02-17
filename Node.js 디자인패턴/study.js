90a7734ef9fba0daeb37f4d8a99a8aa494c5105d
LKH <gorloom6425@naver.com>
2021-02-18 01:03:41 +0900

    1장 정리

--- Node.js 디자인패턴/study.js
+++ Node.js 디자인패턴/study.js
@@ -1,36 +1,4 @@
-const level = require('level');
-const timestamp = require('monotonic-timestamp');
-const JSONStream = require('JSONStream');
-const amqp = require('amqplib');
-const db = level('./msgHistory');
+const numbers = [2, 6, 7, 8, 1];
+const even = numbers.filter(x => x % 2 === 0);
+console.log(even);
 
-require('http').createServer((req, res) => {
-    res.writeHead(200);
-    db.createValueStream()
-        .pipe(JSONStream.stringify())
-        .pipe(res);
-}).listen(8090);
-
-let channel, queue;
-amqp
-    .connect('amqp://localhost') // 1.
-    .then(conn => conn.createChannel())
-    .then(ch => {
-        channel = ch;
-        return channel.assertExchange('chat', 'fanout'); // 2.
-    })
-    .then(() => channel.assertQueue('chat_history')) // 3.
-    .then((q) => {
-        queue = q.queue;
-        return channel.bindQueue(queue, 'chat'); // 4.
-    })
-    .then(() => {
-        return channel.consume(queue, msg => { // 5.
-            const content = msg.content.toString();
-            console.log(`Saving message: ${content}`);
-            db.put(timestamp(), content, err => {
-                if (!err) channel.ack(msg);
-            });
-        });
-    })
-    .catch(err => console.log(err));
