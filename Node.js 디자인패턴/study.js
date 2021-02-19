78c4cd91d4344a2dc7a33730cd76c7047a38e9c2
LKH <gorloom6425@naver.com>
2021-02-19 21:10:06 +0900

    2장 정리

--- Node.js 디자인패턴/study.js
+++ Node.js 디자인패턴/study.js
@@ -1,4 +1,8 @@
-const numbers = [2, 6, 7, 8, 1];
-const even = numbers.filter(x => x % 2 === 0);
-console.log(even);
+asyncFoo(err => {
+    asyncBar(err => {
+        asyncFooBar(err => {
+            // ...
+        });
+    });
+});
 
