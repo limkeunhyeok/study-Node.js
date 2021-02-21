5f59ddfe2adef7ecdeb9cdec43c3281ed5557d1f
LKH <gorloom6425@naver.com>
2021-02-22 00:21:52 +0900

    4장 정리

--- Node.js 디자인패턴/study.js
+++ Node.js 디자인패턴/study.js
@@ -1,8 +1,10 @@
-asyncFoo(err => {
-    asyncBar(err => {
-        asyncFooBar(err => {
-            // ...
-        });
-    });
-});
+function* fruitGenerator() {
+    yield 'apple';
+    yield 'orange';
+    return 'watermelon';
+}
 
+const newFruitGenerator = fruitGenerator();
+console.log(newFruitGenerator.next()); // 1. { value: 'apple', done: false }
+console.log(newFruitGenerator.next()); // 2. { value: 'orange', done: false }
+console.log(newFruitGenerator.next()); // 3. { value: 'watermelon', done: true }
