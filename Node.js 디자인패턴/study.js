f6507ca4e63781c24c17634d0d955138181b5082
LKH <gorloom6425@naver.com>
2021-02-28 16:54:09 +0900

    5장 정리

--- Node.js 디자인패턴/study.js
+++ Node.js 디자인패턴/study.js
@@ -1,10 +1,21 @@
-function* fruitGenerator() {
-    yield 'apple';
-    yield 'orange';
-    return 'watermelon';
+function decorate(component) {
+    const proto = Object.getPrototypeOf(component);
+
+    function Decorator(component) {
+        this.component = component;
+    }
+    Decorator.prototype = Object.create(proto);
+
+    // 새 메소드
+    Decorator.prototype.greetings = function() {
+        return 'Hi!';
+    };
+
+    // 위임된 메소드
+    Decorator.prototype.hello = function() {
+        return this.component.hello.apply(this.component, arguments);
+    };
+
+    return new Decorator(component);
 }
 
-const newFruitGenerator = fruitGenerator();
-console.log(newFruitGenerator.next()); // 1. { value: 'apple', done: false }
-console.log(newFruitGenerator.next()); // 2. { value: 'orange', done: false }
-console.log(newFruitGenerator.next()); // 3. { value: 'watermelon', done: true }
