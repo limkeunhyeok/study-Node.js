137323bb69f2532876e562cca95b1877507d8461
LKH <gorloom6425@naver.com>
2021-03-05 00:21:52 +0900

    7장 정리

--- Node.js 디자인패턴/study.js
+++ Node.js 디자인패턴/study.js
@@ -1,21 +1,9 @@
 function decorate(component) {
-    const proto = Object.getPrototypeOf(component);
-
-    function Decorator(component) {
-        this.component = component;
-    }
-    Decorator.prototype = Object.create(proto);
-
     // 새 메소드
-    Decorator.prototype.greetings = function() {
-        return 'Hi!';
-    };
+    component.greetings = () => {
+        // ...
+    }
 
-    // 위임된 메소드
-    Decorator.prototype.hello = function() {
-        return this.component.hello.apply(this.component, arguments);
-    };
-
-    return new Decorator(component);
+    return component;
 }
 
